063b89b78e806df529ccfaf8587ef9638fe93b23
davis <dbh937@gmail.com>
2015-05-19 09:04:13 -0400

    added loop to run with different salarites at once

--- StarbucksSim.java
+++ StarbucksSim.java
@@ -4,30 +4,31 @@ import java.util.Random;
 
 public class StarbucksSim {
 	public static void main(String[] args) {
-		Store star = new Store(100);
-		Random ran = new Random();
-		
-		Customer[] customers = new Customer[100];
-		for (int i = 0; i < customers.length; i++) {
-			customers[i] = new Customer();
-		}
+		while (true) {
+			Store star = new Store(100);
+			Random ran = new Random();
 
-		int rounds = 100;
-		for (int j = 0; j < rounds; j++) {
-			star.payDay();
-			for (Customer c : customers) {
-				/*
-				 *  int order = ran.nextInt(baristas.placeOfEmployment.menu.size());
-					Product p = baristas.placeOfEmployment.menu.get(order);
-				 * */
-				
-				int b = ran.nextInt(star.baristas.size());
-				Employee barista = star.baristas.get(b);
-				
-				c.placeOrder(barista);
-				//System.out.println(c);
+			Customer[] customers = new Customer[100];
+			for (int i = 0; i < customers.length; i++) {
+				customers[i] = new Customer();
 			}
-			System.out.println("Round number " + j + ": " + star);
+
+			int rounds = 100;
+			double[] record = new double[rounds];
+			for (int j = 0; j < rounds; j++) {
+				star.payDay();
+				for (Customer c : customers) {
+					int b = ran.nextInt(star.baristas.size());
+					Employee barista = star.baristas.get(b);
+
+					c.placeOrder(barista);
+					//System.out.println(c);
+				}
+				record[j] = star.bank;
+				System.out.println("Round number " + j + ": " + star);
+			}
+//			System.out.println(record[record.length-1]);
+//			System.out.println(star.orderHistory);
 		}
 
 	}
