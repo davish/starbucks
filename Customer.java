eb7c490e0567c257bd95663da754566f7f9c86ff
davis <dbh937@gmail.com>
2015-05-19 09:03:12 -0400

    added customer happiness

--- Customer.java
+++ Customer.java
@@ -3,29 +3,36 @@ import java.util.Random;
 
 
 public class Customer {
-	
+
 	public Random ran = new Random();
-	
+	private int happiness = 9;
+
 	/** the amount the customer has paid in his lifetime */
 	public int totalPaid;
-	
+
 	/**
 	 * pick product from the store's menu.
 	 * add cost to totalPaid
 	 * @return the product to be placed
 	 */
 	public Product placeOrder(Employee baristas) {
-		
+
 		int order = ran.nextInt(baristas.placeOfEmployment.menu.size());
-	
+
 		Product p = baristas.placeOfEmployment.menu.get(order);
 		totalPaid += p.price;
-		
-		baristas.processOrder(p);
-		
+		boolean orderProcessed;
+		double r = Math.random();
+		if ((double)happiness/10 >= r) {
+			orderProcessed = baristas.processOrder(p);
+			if (orderProcessed)
+				happiness++;
+			else
+				happiness--;
+		}
 		return p;
 	}
-	
+
 	public String toString() {
 		return totalPaid + "";
 	}
